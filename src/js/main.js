6847c4ae30230e92f80dbb279eebf5ad470b9af7
Ashleigh <ashleighc207@gmail.com>
2019-06-15 16:39:59 -0400

    add in hover effect for project tiles:

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,22 @@
-$('.project-tile-image').on('mouseover', function(e){
-	let link = "";
-	if($(e.target).hasClass('project-tile')){
-		$(e.target).addClass('active')
-		link = $(e.target).children('.project-links');
-	} else {
-		$(e.target).addClass('active')
-		link = $(e.target).siblings('.project-links');
-	}
-	$(link).show()
-})
+// $('.project-tile-image').on('mouseover', function(e){
+// 	let link = "";
+// 	if($(e.target).hasClass('project-tile')){
+// 		$(e.target).addClass('active')
+// 		link = $(e.target).children('.project-links');
+// 	} else {
+// 		$(e.target).addClass('active')
+// 		link = $(e.target).siblings('.project-links');
+// 	}
+// 	$(link).show()
+// })
 
-$('.project-tile-image').on('mouseout', function(e){
-	let link = "";
-	if($(e.target).hasClass('project-tile')){
-		link = $(e.target).children('.project-links');
-		$(e.target).removeClass('active')
-	} else {
-		link = $(e.target).siblings('.project-links');
-	}
-	$(link).hide()
-})
+// $('.project-tile-image').on('mouseout', function(e){
+// 	let link = "";
+// 	if($(e.target).hasClass('project-tile')){
+// 		link = $(e.target).children('.project-links');
+// 		$(e.target).removeClass('active')
+// 	} else {
+// 		link = $(e.target).siblings('.project-links');
+// 	}
+// 	$(link).hide()
+// })
